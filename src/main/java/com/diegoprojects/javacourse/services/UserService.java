9d657707f715285c3832efeb50ab50d8ad53ca10
diegovscoelho <dcoelhocarneiro@gmail.com>
2025-03-10 20:55:53 -0300

    Service layer, component registration

---
+++ src/main/java/com/diegoprojects/javacourse/services/UserService.java
@@ -0,0 +1,25 @@
+package com.diegoprojects.javacourse.services;
+
+import com.diegoprojects.javacourse.entities.User;
+import com.diegoprojects.javacourse.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
